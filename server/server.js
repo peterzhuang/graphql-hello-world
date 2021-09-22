c03588cf1da2d8d20c42d6ec6935a53a299dd765
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2021-09-22 15:46:58 -0400

    graphql hello world

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    schema {
+        query: Query
+    }
+
+    type Query {
+        greeting: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        greeting: () => 'Hello GraphQL world'
+    }
+};
+
+const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen({port: 9000})
+    .then(({ url }) => console.log(`Server running at ${url}`));
